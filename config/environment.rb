da67e363d193eaf39f95ceac88fc99ab9b6ddd1d
yoon <yoon@yoons-macbook-air.local>
2008-10-15 14:19:21 -0500

    PubMed and MEDLINE efetch via BioRuby gem

--- config/environment.rb
+++ config/environment.rb
@@ -25,7 +25,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "rspec-rails", :lib => "spec" 
+  config.gem "rspec-rails", :lib => "spec"
+  config.gem "bio", :version => "1.2.1"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
