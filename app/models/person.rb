634a6ace1b1d82091f52bb66a8a44f5347f2bd97
yoon <yoon@yoons-macbook-air.local>
2008-10-15 12:55:04 -0500

    added revu total and revu totals for person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,24 @@
 class Person < ActiveRecord::Base
+  has_many :revus
+  
+  def revu_totals
+    total = 0.0
+    revu_subtotals.map{|d_st| [ d_st[0], total+=d_st[1] ]}
+  end
+  def revu_total(date = Date.today)
+    revu_subtotals.inject(0.0) {|sum,d_st| d_st[0].to_date <= date.to_date ? sum + d_st[1] : sum }
+  end
+
+  private
+  
+  def revu_subtotals
+    h = {}
+    revus.each do |revu|
+      h[revu.starts_on] ||= 0.0
+      h[revu.starts_on] += revu.score
+      h[revu.ends_on+1] ||= 0.0
+      h[revu.ends_on+1] -= revu.score
+    end
+    h.sort
+  end
 end
