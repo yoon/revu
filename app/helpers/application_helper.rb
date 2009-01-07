650aa944b570506f15850da230b33e14e5f5270b
yoon <yoon@northwestern.edu>
2009-01-07 15:24:33 -0600

    title display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def crumb_helper(text=nil, link=nil)
+    @breadcrumbs = (link.nil? ? text : link_to(text, link))
+  end
+  def title_helper(text=nil, link=nil)
+    @title = (link.nil? ? text : link_to(text, link))
+  end
+  def tab_helper(text, link=nil)
+    @tabs ||= []
+    @tabs << (link.nil? ? "<li class='current'>#{text}</li>" : "<li>#{link_to(text, link)}</li>")
+  end
+  def javascripts_helper(*args)
+    @javascripts = args
+  end
+  def stylesheets_helper(*args)
+    @stylesheets = args
+  end
 end
