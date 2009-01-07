f690f068e4f28a2e8fd6a328c82dd7f22e9f8f23
yoon <yoon@northwestern.edu>
2009-01-07 14:51:19 -0600

    fixes and improvments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  req = {:activity_type => /grant|publication|presentation|service/}
   get, put, pst, del = {:method => :get}, {:method => :put}, {:method => :post}, {:method => :delete}
   
   map.with_options :controller => 'activities' do |map|
-    map.activities        '/',                                  :conditions => get, :action => "index"
-    map.show_activity     '/:activity_type/:activity_id',       :conditions => get, :action => "show"
-    map.new_activity      '/:activity_type',                    :conditions => get, :action => "new"
-    map.create_activity   '/:activity_type',                    :conditions => pst, :action => "create"
-    map.edit_activity     '/:activity_type/:activity_id/edit',  :conditions => get, :action => "edit"
-    map.update_activity   '/:activity_type/:activity_id',       :conditions => put, :action => "update"
-    map.destroy_activity  '/:activity_type/:activity_id',       :conditions => del, :action => "destroy"
+    map.activities        '/activities/',                       :conditions => get,                       :action => "index"
+    map.show_activity     '/:activity_type/:activity_id',       :conditions => get, :requirements => req, :action => "show"
+    map.new_activity      '/:activity_type',                    :conditions => get, :requirements => req, :action => "new"
+    map.create_activity   '/:activity_type',                    :conditions => pst, :requirements => req, :action => "create"
+    map.edit_activity     '/:activity_type/:activity_id/edit',  :conditions => get, :requirements => req, :action => "edit"
+    map.update_activity   '/:activity_type/:activity_id',       :conditions => put, :requirements => req, :action => "update"
+    map.destroy_activity  '/:activity_type/:activity_id',       :conditions => del, :requirements => req, :action => "destroy"
   end
 end
