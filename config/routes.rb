30f20849f936969fba5913b4020914ae3693b973
yoon <yoon@northwestern.edu>
2009-01-08 12:10:15 -0600

    restful authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.resources :users
+
+  map.resource :session
+
   req = {:activity_type => /grants|publications|presentations|services/}
   get, put, pst, del = {:method => :get}, {:method => :put}, {:method => :post}, {:method => :delete}
   
